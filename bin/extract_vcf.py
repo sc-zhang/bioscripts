eca736bee2261be67ceda902ad5f95f1e75017d3
sc-zhang <zsc-zhang@foxmail.com>
2025-06-06 14:55:46 +0800

    fix bug

--- bin/extract_vcf.py
+++ bin/extract_vcf.py
@@ -6,7 +6,7 @@ def bin_search(bed_list, pos):
 	s = 0
 	e = len(bed_list)-1
 	while s <= e:
-		mid = (s+e)/2
+		mid = (s+e)//2
 		if bed_list[mid][0] < pos:
 			s = mid+1
 		elif bed_list[mid][0] > pos:
