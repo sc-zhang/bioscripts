a85e3ce08e7aba65fb78e1f58da8240936320c5e
sc-zhang <zsc-zhang@foxmail.com>
2025-11-27 09:59:46 +0800

    update easyGoKegg

--- bin/easyGoKegg.R
+++ bin/easyGoKegg.R
@@ -1,46 +1,49 @@
 #!/usr/bin/env Rscript
 library(optparse)
-
+library(clusterProfiler)
+library(KEGGREST)
+library(dplyr)
+library(stringr)
+library(AnnotationForge)
+library(jsonlite)
+library(purrr)
+library(RCurl)
+library(ggplot2)
 
 download_plant_path <- function(db_file) {
   ### Download plant pathway
-  library(KEGGREST)
   org <- data.frame(keggList("organism"))
   plants <- org[grep("Plants", org$phylogeny), ]
   plant_pathways_total <- vector()
 
-  for (i in 1:length(plants$organism))
-  {
+  for (i in seq_along(length(plants$organism))) {
     try({
       pathways <- keggLink("pathway", plants[i, 2])
       pathways <- sub(paste(".*", plants[i, 2], sep = ""), "", pathways)
       pathways <- unique(pathways)
-      if ("03250" %in% pathways) {
-        print(plants[i, 2])
-      }
       plant_pathways_total <- append(plant_pathways_total, pathways)
       plant_pathways_total <- unique(plant_pathways_total)
     })
   }
 
   plant_pathways_total <- paste0("ko", plant_pathways_total)
-  write.table(plant_pathways_total, file = db_file, quote = F, row.names = F, col.names = F)
+  write.table(plant_pathways_total, file = db_file, quote = FALSE, row.names = FALSE, col.names = FALSE)
 }
 
 load_plant_path <- function(db_file) {
   ### Load plant pathway
-  plant_pathways_total <- read.table(db_file, sep = "", header = F)
+  plant_pathways_total <- read.table(db_file, sep = "", header = FALSE)
   colnames(plant_pathways_total) <- "Pathway"
   plant_pathways_total
 }
 
 generate_go_db <- function(annotation_file, db_path, genus, species, tax_id) {
   cat("Reading annotation file\n")
-  egg <- read.table(annotation_file, header = T, sep = "\t", quote = "")
+  egg <- read.table(annotation_file, header = TRUE, sep = "\t", quote = "")
   egg[egg == ""] <- NA
 
   gterms <- egg %>%
-    dplyr::select(query, GOs) %>%
+    dplyr::select("query", "GOs") %>%
     na.omit()
 
   gene2go <- data.frame(
@@ -61,11 +64,11 @@ generate_go_db <- function(annotation_file, db_path, genus, species, tax_id) {
   )
 
   gene_info <- egg %>%
-    dplyr::select(GID = query, GENENAME = Preferred_name) %>%
+    dplyr::select(GID = "query", GENENAME = "Preferred_name") %>%
     na.omit()
 
   gene2ko <- egg %>%
-    dplyr::select(GID = query, Ko = KEGG_ko) %>%
+    dplyr::select(GID = "query", Ko = "KEGG_ko") %>%
     na.omit()
 
   gene2ko$Ko <- gsub("ko:", "", gene2ko$Ko)
@@ -92,10 +95,14 @@ generate_go_db <- function(annotation_file, db_path, genus, species, tax_id) {
   return(anno_db)
 }
 
-update_kegg_db <- function(db_path, kegg_db_file) {
+update_kegg_db <- function(db_path, kegg_json, kegg_db_file) {
   url <- "https://www.kegg.jp/kegg-bin/download_htext?htext=ko00001&format=json&filedir="
-  json <- paste(db_path, "ko00001.json", sep = "/")
-  download.file(url, json)
+  if (!file.exists(kegg_json)) {
+    json <- paste(db_path, "ko00001.json", sep = "/")
+    download.file(url, json)
+  } else {
+    json <- kegg_json
+  }
   pathway2name <- tibble(Pathway = character(), Name = character())
   ko2pathway <- tibble(Ko = character(), Pathway = character())
   kegg <- fromJSON(json)
@@ -118,12 +125,13 @@ update_kegg_db <- function(db_path, kegg_db_file) {
 }
 
 load_kegg_db <- function(kegg_db_file, gene2ko) {
-  load(file = kegg_db_file)
+  kegg_db <- new.env()
+  load(file = kegg_db_file, envir = kegg_db)
   gene2pathway <- gene2ko %>%
-    left_join(ko2pathway, by = "Ko", relationship = "many-to-many") %>%
-    dplyr::select(GID, Pathway) %>%
+    left_join(kegg_db$ko2pathway, by = "Ko", relationship = "many-to-many") %>%
+    dplyr::select("GID", "Pathway") %>%
     na.omit()
-  kegg_db <- list(pathway2name = pathway2name, gene2pathway = gene2pathway)
+  kegg_db <- list(pathway2name = kegg_db$pathway2name, gene2pathway = gene2pathway)
   return(kegg_db)
 }
 
@@ -134,13 +142,13 @@ keep_plant_path_only <- function(pathway2name, gene2pathway, plant_pathways_tota
   return(filtered_kegg_db)
 }
 
-run_go_kegg <- function(db_path, gene2pathway, pathway2name, gene_file, 
+run_go_kegg <- function(db_path, gene2pathway, pathway2name, gene_file,
                         pvalue_cutoff, qvalue_cutoff, padjust_method, ontology) {
-  gene <- read.table(gene_file, header = F)
+  gene <- read.table(gene_file, header = FALSE)
   gene_list <- gene[, 1]
   GO <- enrichGO(
     gene = gene_list,
-    OrgDb = org.CCUSTOM.eg.db,
+    OrgDb = "org.CCUSTOM.eg.db",
     keyType = "GID",
     ont = "ALL",
     pAdjustMethod = padjust_method,
@@ -150,7 +158,7 @@ run_go_kegg <- function(db_path, gene2pathway, pathway2name, gene_file,
 
   cat("Saving GO result\n")
   GO_df <- as.data.frame(GO)
-  write.table(GO_df, file = "GO.results.txt", quote = F)
+  write.table(GO_df, file = "GO.results.txt", quote = FALSE)
 
   cat("Saving GO barplot\n")
   pdf(file = "GO_barplot.pdf", width = 15, height = 20)
@@ -174,11 +182,11 @@ run_go_kegg <- function(db_path, gene2pathway, pathway2name, gene_file,
 
   cat("Saving KEGG result\n")
   KEGG_df <- as.data.frame(KEGG)
-  write.table(KEGG_df, file = "KEGG.results.txt", quote = F)
+  write.table(KEGG_df, file = "KEGG.results.txt", quote = FALSE)
 
   cat("Saving KEGG barplot\n")
   pdf(file = "KEGG_barplot.pdf", width = 15, height = 20)
-  print(barplot(KEGG, drop = TRUE, showCateKEGGry = 10))
+  print(barplot(KEGG, drop = TRUE, showCategory = 10))
   dev.off()
 
   cat("Saving KEGG bubble\n")
@@ -191,15 +199,6 @@ main_pipe <- function() {
   opt <- parse_args(opt_parser)
   opt_names <- names(opt)
   if ("input" %in% opt_names && "anno" %in% opt_names && "db" %in% opt_names) {
-    library(clusterProfiler)
-    library(dplyr)
-    library(stringr)
-    library(AnnotationForge)
-    library(jsonlite)
-    library(purrr)
-    library(RCurl)
-    library(ggplot2)
-
     anno_file <- opt$anno
     db_path <- opt$db
     if (!dir.exists(db_path)) {
@@ -219,18 +218,21 @@ main_pipe <- function() {
     }
 
     if (file.exists(db_pack)) {
-      unlink(db_pack, recursive = T)
+      unlink(db_pack, recursive = TRUE)
     }
     anno_db <- generate_go_db(anno_file, db_path, opt$genus, opt$species, opt$tax_id)
 
     cat("Loading GO database\n")
     install.packages(db_pack, repos = NULL, type = "sources")
     do.call(library, list(go_db_name))
-
+    kegg_json <- ""
+    if ("kegg_json" %in% opt_names) {
+      kegg_json <- opt$kegg_json
+    }
     kegg_db_file <- paste(db_path, "KEGG_db.RData", sep = "/")
     if (!file.exists(kegg_db_file) || "update" %in% opt_names) {
       cat("Generating KEGG database\n")
-      update_kegg_db(db_path, kegg_db_file)
+      update_kegg_db(db_path, kegg_json, kegg_db_file)
     }
 
     cat("Loading KEGG database\n")
@@ -238,9 +240,10 @@ main_pipe <- function() {
 
     if ("plant" %in% opt_names) {
       cat("Keeping plants pathway\n")
-      plants_kegg <- "plants.kegg.txt"
-      plants_kegg_file <- paste(db_path, plants_kegg, sep = "/")
+      plants_kegg_file <- opt$plant_kegg
       if (!file.exists(plants_kegg_file)) {
+        plants_kegg <- "plants.kegg.txt"
+        plants_kegg_file <- paste(db_path, plants_kegg, sep = "/")
         download_plant_path(plants_kegg_file)
       }
       plant_pathways_total <- load_plant_path(plants_kegg_file)
@@ -249,8 +252,10 @@ main_pipe <- function() {
 
     gene_file <- opt$input
     cat("Running GO and KEGG\n")
-    run_go_kegg(db_path, kegg_db$gene2pathway, kegg_db$pathway2name, gene_file,
-                pvalue_cutoff, qvalue_cutoff, padjust_method, ontology)
+    run_go_kegg(
+      db_path, kegg_db$gene2pathway, kegg_db$pathway2name, gene_file,
+      pvalue_cutoff, qvalue_cutoff, padjust_method, ontology
+    )
 
     cat("Finished\n")
   } else {
@@ -262,6 +267,7 @@ opt_list <- list(
   make_option(c("-i", "--input"), type = "character", help = "Input gene list file"),
   make_option(c("-a", "--anno"), type = "character", help = "Functional annotation file"),
   make_option(c("-d", "--db"), type = "character", help = "Database path"),
+  make_option(c("--kegg_json"), type = "character", help = "Pre-downloaded kegg json file"),
   make_option(c("--genus"),
     type = "character", help = "Genus name for creating GO database, default=\"Custom genus\"",
     default = "Custom genus"
@@ -285,7 +291,8 @@ opt_list <- list(
     default = "0000"
   ),
   make_option(c("--update"), action = "store_true", help = "Update databases"),
-  make_option(c("--plant"), action = "store_true", help = "enrich with plant pathway only")
+  make_option(c("--plant"), action = "store_true", help = "enrich with plant pathway only"),
+  make_option(c("--plant_kegg"), type = "character", help = "Pre-generated plant kegg db file")
 )
 
 opt_parser <- OptionParser(option_list = opt_list, usage = "This Script is used for running GO and KEGG")
